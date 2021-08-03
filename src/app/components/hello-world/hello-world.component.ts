2afa951e78d7b2d1c97221dde1ce6004b979d045
learnshare <learnshare@126.com>
2021-08-03 11:01:29 +0800

    hello world.

---
+++ src/app/components/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+})
+export class HelloWorldComponent {
+  @Input() msg = '';
+}
