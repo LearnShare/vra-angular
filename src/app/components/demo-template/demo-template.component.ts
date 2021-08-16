31637c82042f67dabc520e90ca783d449409afea
learnshare <learnshare@126.com>
2021-08-16 12:57:13 +0800

    update(demo): template.

--- src/app/components/demo-template/demo-template.component.ts
+++ src/app/components/demo-template/demo-template.component.ts
@@ -8,12 +8,14 @@ export class DemoTemplateComponent {
   msg: string;
   a: number;
   b: number;
+  imgUrl: string;
   htmlContent: string;
 
   constructor() {
     this.msg = 'value from data';
     this.a = 1;
     this.b = 2;
+    this.imgUrl = 'https://images.unsplash.com/photo-1629019625736-89ff131a723c?w=160&q=80';
     this.htmlContent = '<strong>html content</strong>';
   }
 }
