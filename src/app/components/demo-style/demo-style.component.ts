e79bb5647938a14b04d1cd3a3f69f438dea1f903
learnshare <learnshare@126.com>
2021-08-18 22:58:44 +0800

    (demo): style.

---
+++ src/app/components/demo-style/demo-style.component.ts
@@ -0,0 +1,53 @@
+import {
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-demo-style',
+  templateUrl: './demo-style.component.html',
+  styleUrls: [
+    './demo-style.component.scss',
+  ],
+})
+export class DemoStyleComponent {
+  color: string;
+  fontSize: string;
+  bgColor: string;
+
+  styleString: string;
+  styleObject: {
+    [key: string]: any;
+  };
+
+  classString: string;
+  classList: Array<string>;
+  classObject: {
+    [key: string]: any;
+  };
+
+  constructor() {
+    this.color = '#AAF';
+    this.fontSize = '150%';
+    this.bgColor = '#CCF';
+
+    const styles = [
+      'font-size: 16px',
+      'color: #F66',
+    ];
+    this.styleString = styles.join(';');
+    this.styleObject = {
+      'font-size': '16px',
+      color: '#333',
+    };
+
+    this.classString = 'info success';
+    this.classList = [
+      'info',
+      'warning',
+    ];
+    this.classObject = [
+      'info',
+      'danger',
+    ];
+  }
+}
